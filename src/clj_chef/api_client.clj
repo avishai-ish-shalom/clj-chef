0f9607379399529e19586fb3f1d10661cdd4e293
Avishai Ish-Shalom <avishai@fewbytes.com>
2013-07-11 18:36:42 +0300

    minor line reordering

--- src/clj_chef/api_client.clj
+++ src/clj_chef/api_client.clj
@@ -25,12 +25,12 @@
 ; defrest is in clj-chef.rest namespace
 (defrest :node "/nodes/:id" [:get :delete :put])
 (defrest :client "/clients/:id" [:get :delete :put])
-(defrest :cookbook "/cookbooks/:id" [:get :delete :put])
 (defrest :role "/roles/:id" [:get :delete :put])
 (defrest :environment "/environments/:id" [:get :delete :put])
 (defrest :environment-node "/environments/:id/nodes" [:get])
 (defrest :data-bag "/data/:id" [:get :delete :put])
 (defrest :data-bag-item "/data/:bag/:item" [:get :delete :put])
+(defrest :cookbook "/cookbooks/:id" [:get :delete :put])
 (defrest :cookbook-version "/cookbooks/:name/:version" [:get])
 
 (defn search
